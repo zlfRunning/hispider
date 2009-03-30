1d41b61ffa86375ecb26827cae2174acacabf500
sounos <sounos@fa9b0471-584b-0410-83a0-051e69a370e1>
2009-03-30 10:03:38 +0000

    updated hibase.h

--- devel/task/hibase.h
+++ devel/task/hibase.h
@@ -6,11 +6,12 @@
 #define FTYPE_ALL 		(FTYPE_INT|FTYPE_FLOAT|FTYPE_TEXT)	
 #define FIELD_NUM_MAX		256
 #define FIELD_NAME_MAX		256
-#define REGX_SIZE_MAX		65536
+#define HIBASE_PATH_MAX		256
+#define REGX_SIZE_MAX		4096
 /* field */
 typedef struct _IFILED
 {
-	short  type;
+	short  data_type;
 	short  template_id;
 	int    off;
 	int    len;
@@ -18,7 +19,6 @@ typedef struct _IFILED
 /* table */
 typedef struct _ITABLE
 {
-
 	short id;
 	short nfields;
 	IFIELD fields[FIELD_NUM_MAX];
@@ -29,15 +29,31 @@ typedef struct _IREGX
 {
 	short table_id;
 	short field_id;
-	
 }IREGX;
 /* template */
 typedef struct _ITEMPLATE
 {
-	short docid;
+	short status;
 	short nfields;
 	IREGX map[FIELD_NAME_MAX];
 	char  regx[REGX_SIZE_MAX];
-
 }ITEMPLATE;
+/* hibase */
+typedef struct _HIBASE
+{
+	void *table_map;
+	void *template_map;
+	char basedir[HIBASE_PATH_MAX];
+
+	int 	(*set_basedir)(struct _HIBASE *, char *dir);
+	int 	(*add_table)(struct _HIBASE *, ITABLE *tab);
+	int	(*get_table)(struct _HIBASE *, int table_id, char *table_name);
+	int 	(*update_table)(struct _HIBASE *, int table_id, ITABLE *tab);
+	int 	(*delete_table)(struct _HIBASE *, int table_id);
+	int 	(*add_template)(struct _HIBASE *, ITEMPLATE *);
+	int 	(*get_template)(struct _HIBASE *, int template_id, char *template_name);
+	int 	(*update_template)(struct _HIBASE *, int template_id, ITEMPLATE *);
+	int 	(*delete_template)(struct _HIBASE *, int template_id, ITEMPLATE *);
+	void 	(*clean)(struct _HIBASE **);	
+}HIBASE;
 #endif
