c2f77126fe62837faf4d0056ebd885b78454fa9c
sounos <sounos@fa9b0471-584b-0410-83a0-051e69a370e1>
2009-03-30 04:15:39 +0000

    update hibase.h

--- devel/task/hibase.h
+++ devel/task/hibase.h
@@ -6,6 +6,8 @@
 #define FTYPE_ALL 		(FTYPE_INT|FTYPE_FLOAT|FTYPE_TEXT)	
 #define FIELD_NUM_MAX		256
 #define FIELD_NAME_MAX		256
+#define REGX_SIZE_MAX		65536
+/* field */
 typedef struct _IFILED
 {
 	short  type;
@@ -13,6 +15,7 @@ typedef struct _IFILED
 	int    off;
 	int    len;
 }IFIELD;
+/* table */
 typedef struct _ITABLE
 {
 
@@ -21,4 +24,20 @@ typedef struct _ITABLE
 	IFIELD fields[FIELD_NUM_MAX];
 	char name[FIELD_NAME_MAX];
 }ITABLE;
+/* regular expression */
+typedef struct _IREGX
+{
+	short table_id;
+	short field_id;
+	
+}IREGX;
+/* template */
+typedef struct _ITEMPLATE
+{
+	short docid;
+	short nfields;
+	IREGX map[FIELD_NAME_MAX];
+	char  regx[REGX_SIZE_MAX];
+
+}ITEMPLATE;
 #endif
